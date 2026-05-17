4a516c6a284cbfb84d7b046178010730907fef0d
Martin Klein <36695017+flitzrrr@users.noreply.github.com>
2026-05-17 10:15:01 +0200

    feat(corpus): KI-Verordnung / AI Act (CELEX 32024R1689) loader
    Signed-off-by: Martin Klein <36695017+flitzrrr@users.noreply.github.com>

--- prepare_data/eu_acts/__init__.py
+++ prepare_data/eu_acts/__init__.py
@@ -3,9 +3,10 @@
 """EUR-Lex / Cellar adapters for EU acts."""
 
 from prepare_data.eu_acts._normalize import adapt_eu_act
+from prepare_data.eu_acts.ai_act import AIAct
 from prepare_data.eu_acts.cellar_client import CellarClient
 from prepare_data.eu_acts.dma import DMAAct
 from prepare_data.eu_acts.dsa import DSAAct
 from prepare_data.eu_acts.eprivacy import EPrivacyAct
 
-__all__ = ["CellarClient", "DMAAct", "DSAAct", "EPrivacyAct", "adapt_eu_act"]
+__all__ = ["AIAct", "CellarClient", "DMAAct", "DSAAct", "EPrivacyAct", "adapt_eu_act"]
