e64d444444a5e137e4710af6b016a4dec407a454
Martin Klein <36695017+flitzrrr@users.noreply.github.com>
2026-05-17 10:14:10 +0200

    feat(corpus): Digital Markets Act (CELEX 32022R1925) loader
    Signed-off-by: Martin Klein <36695017+flitzrrr@users.noreply.github.com>

--- prepare_data/eu_acts/__init__.py
+++ prepare_data/eu_acts/__init__.py
@@ -4,7 +4,8 @@
 
 from prepare_data.eu_acts._normalize import adapt_eu_act
 from prepare_data.eu_acts.cellar_client import CellarClient
+from prepare_data.eu_acts.dma import DMAAct
 from prepare_data.eu_acts.dsa import DSAAct
 from prepare_data.eu_acts.eprivacy import EPrivacyAct
 
-__all__ = ["CellarClient", "DSAAct", "EPrivacyAct", "adapt_eu_act"]
+__all__ = ["CellarClient", "DMAAct", "DSAAct", "EPrivacyAct", "adapt_eu_act"]
